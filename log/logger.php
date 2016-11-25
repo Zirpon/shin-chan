0caf203cfdde18997bc60f51e7230fd3cebe309c
champon <735037139@qq.com>
2016-11-25 19:01:54 +0800

    logger db module

---
+++ log/logger.php
@@ -0,0 +1,40 @@
+<?php
+require_once dirname(__FILE__)."/logger_define.php";
+
+Class logger
+{
+	//define("taobao", LOGGERPATH."taobao_debug.log");
+	//define("jingdong", LOGGERPATH."taobao_debug.log");
+
+	public function write( $log_str, $log_type )
+	{
+		if ($GLOBALS['LOGGER_SWITCH'] === FALSE) {
+			return;	
+		}
+
+		$loggerType = $GLOBALS['LOGGER_TYPE'];
+		var_dump($loggerType);
+
+		if ( !is_null($loggerType[$log_type]) ) 
+		{
+			error_log(date("Y-m-d H:i:s ").$log_str."\n", 3, $loggerType[$log_type] );
+		}
+	}
+
+	public function debug( $log_str, $log_type)
+	{
+		if ($GLOBALS['LOGGER_DEBUG_SWITCH'] === FALSE) {
+			return;	
+		}
+
+		$loggerType = $GLOBALS['LOGGER_TYPE'];
+		var_dump($loggerType);
+
+		if ( !is_null($loggerType[$log_type]) ) 
+		{
+			error_log(date("Y-m-d H:i:s ").$log_str."\n", 3, $loggerType[$log_type] );
+		}
+	}
+}
+
+?>
---
+++ log/logger_define.php
@@ -0,0 +1,14 @@
+<?php
+
+define("LOGGERPATH", "/home/wwwroot/default/shin-chan/");
+	
+$LOGGER_TYPE = array
+(
+	"taobao"	=> LOGGERPATH."taobao_debug.log",
+	"jingdong"	=> LOGGERPATH."jingdong_debug.log"
+);
+
+$LOGGER_SWITCH = TRUE;
+$LOGGER_DEBUG_SWITCH = TRUE;
+
+?>
