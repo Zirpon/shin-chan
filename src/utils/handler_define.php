e50e14979c74c79af829144eb111b0af7647746f
= <=>
2016-12-19 10:45:31 +0800

    comment the function params for client developer check

--- src/utils/handler_define.php
+++ src/utils/handler_define.php
@@ -1,39 +1,39 @@
 <?php
 	$handlers = array(
 		"account"	=> array(
-			2001	=> "newAccount",
-			2002	=> "checkAccount",
-			2003	=> "newChar",
-			2004	=> "login",
-			//2005	=> "bExistsChar",
-			//2006	=> "sGetCharname",
-			2007	=> "guestLogin",
-			2008	=> "archive",
-			2009	=> "loadArchive",
+			2001	=> "newAccount",//$account, $pwd, $type
+			2002	=> "checkAccount",//$account, $pwd, $type
+			2003	=> "newChar",//$account, $type, $charname
+			2004	=> "login",//$account, $type
+			//2005	=> "bExistsChar",//$guid 
+			//2006	=> "sGetCharname",//$guid 
+			2007	=> "guestLogin",//$account, $type, $charname
+			2008	=> "archive",//$guid, $data
+			2009	=> "loadArchive",//$guid
 		),
 
 		"message"	=> array(
-			2011	=> "newMsg",
-			2012	=> "getMsgList",
-		//	2013	=> "getMsg",
-		//	2014	=> "readMsg",
-			2015	=> "handleMsg",
+			2011	=> "newMsg",//$senderid, $receiverid, $type, $content, $deadline 
+			2012	=> "getMsgList",//guid
+		//	2013	=> "getMsg",//msgid
+		//	2014	=> "readMsg",//msgid
+			2015	=> "handleMsg",//guid msgid
 			2016	=> "getTimestamp",
 		),
 
 		"friend"	=> array(
-			2021	=> "createFriend",
-			2022	=> "sendGift",
-			2023	=> "unfriend",
+			2021	=> "createFriend",//$playerid, $friendid
+			2022	=> "sendGift",//$playerid, $playername, $friendid, $friendname 
+			2023	=> "unfriend",//$playerid, $friendid
 			2024	=> "requestGift",
 			2025	=> "sendMsg",
 		),
 
 		"mail"		=> array(
-			2031	=> "sendMail",
-			//2032	=> "loadMail",
-			2033	=> "readMail",
-			2034	=> "loadMailList",
+			2031	=> "sendMail",//$type, $sourceid, $sourcename, $receiverid, $receivername
+			//2032	=> "loadMail",//$mailid
+			2033	=> "readMail",//$playerid, $mailid
+			2034	=> "loadMailList",//$guid
 		),
 	);
 ?>
