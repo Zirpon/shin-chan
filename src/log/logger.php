76780d28c17621c232ffa640d6ab9a354c21fb6b
champon <735037139@qq.com>
2017-02-05 21:12:22 +0800

    邮件subject调试

--- src/log/logger.php
+++ src/log/logger.php
@@ -60,11 +60,7 @@ Class logger
 		if ($log_type == "php_error") {
 
 			foreach ($GLOBALS['LOGGER_MAIL_RECEIVER'] as $value) {
-				error_log(
-						get_server_ip()."\n"."蜡笔小新PHP后台运行时错误"."\n"."handleFatalPhpError:".$log_str, 
-						1, $value,
-						"Subject: 蜡笔小新PHP后台运行时错误\nFrom: dayu@".gethostname()."\n"
-					);
+				error_log(get_server_ip()."\n"."蜡笔小新PHP后台运行时错误"."\n"."handleFatalPhpError:".$log_str, 1, $value);
 			}
 		}
 
